9cdd896057178d2d478a647e86933d1006f0bbff
mvvrachev <martin.v.vrachev.2017@elsys-bg.org>
2021-01-28 17:12:26 +0200

    Added error messages under info boxes in register page

--- flaskforum/form.py
+++ flaskforum/form.py
@@ -1,33 +1,16 @@
 from flask.app import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, EqualTo, ValidationError #Email
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Email
 from flaskforum.models import User
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
-    email = StringField('Email', validators=[DataRequired()]) #should validate email?
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
-    submit = SubmitField('Sign Up')
-
-    '''def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if(user is None):
-            pass
-        else:
-            raise ValidationError("That username is taken. Choose another one.")
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if(email is None):
-            pass
-        else:
-            raise ValidationError("That email is taken. Choose another one.")
-        '''
-
-            
+    submit = SubmitField('Sign Up')            
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()]) #should validate email?
